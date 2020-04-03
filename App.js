bfb193f280138ef3d2e132b3ffdac36b8abf4f74
Matias Perez Hughes <hughesmatias@gmail.com>
2020-04-03 00:22:53 -0300

    datepicker to set date when we are creating a new task

--- App.js
+++ App.js
@@ -9,8 +9,14 @@ import {
   Button,
 } from 'react-native';
 
+import DatePicker from 'react-native-datepicker';
+
+import moment from 'moment';
+
 export default function App() {
+  const globalFormat = "YYYY-MM-DD";
   const [ newTask, setNewTask ] = useState("");
+  const [ newDate, setNewDate ] = useState(moment().format(globalFormat));
   const [ tasks, setTask ] = useState([
     {
       id: 1,
@@ -28,7 +34,7 @@ export default function App() {
       id: 4,
       description: "Task 4"
     }
-  ])
+  ]);
 
   const removeTask = (id) => {
     setTask(
@@ -36,26 +42,52 @@ export default function App() {
     )
   }
 
-  const onChangeHandle = text => setNewTask(text)
+  const onChangeHandle = text => setNewTask(text);
 
   const addTask = () => {
     tasks.push({
       id: Math.random(),
       description: newTask,
+      date: newDate,
     });
+    tasks.sort((t1, t2) => 
+      moment(t1.date ? t1.date : moment()).isAfter(moment(t2.date ? t2.date : moment()))? 1: -1);
     setNewTask("");
+    setNewDate(moment().format(globalFormat));
   }
 
   const isEmptyNewTaskInput = () => newTask.length > 0 ? false : true;
 
+  const datePickerText = {
+    confirm: 'Confirm',
+    cancel: 'Cancel',
+    placeholder: 'select date',
+  };
+
+  const onDateHandle = date => {setNewDate(date)};
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>List of tasks</Text>
-      <View>
+      <View style={styles.formNewTask}>
         <TextInput
+          style={styles.taskElement}
           placeholder="Write your task."
           onChangeText={onChangeHandle}
           value={newTask}
+        />
+        <DatePicker
+          style={{width: 200}}
+          date={newDate}
+          mode="date"
+          placeholder={datePickerText.placeholder}
+          format={globalFormat}
+          minDate={moment().format(globalFormat)}
+          maxDate={moment().add(1, "year").format(globalFormat)}
+          confirmBtnText={datePickerText.confirm}
+          cancelBtnText={datePickerText.cancel}
+          customStyles={datePickerStyles}
+          onDateChange={onDateHandle}
         />
         <Button
           title="Save new Task"
@@ -64,7 +96,7 @@ export default function App() {
         />
       </View>
       {
-        tasks == 0 ?
+        tasks.length == 0 ?
         <Text style={styles.emptyList}>You dont have any task.</Text>
         :
         <FlatList
@@ -73,6 +105,7 @@ export default function App() {
           renderItem={({ item }) =>
             <TouchableOpacity style={styles.taskElement} onPress={() => removeTask(item.id)}>
               <Text>{item.description}</Text>
+              {item.date ? <Text>{item.date}</Text> : null}
             </TouchableOpacity>
           }
           keyExtractor={(item) => item.id.toString()}
@@ -82,6 +115,18 @@ export default function App() {
   );
 }
 
+const datePickerStyles = StyleSheet.create({
+  dateIcon: {
+    position: 'absolute',
+    left: 0,
+    top: 4,
+    marginLeft: 0
+  },
+  dateInput: {
+    marginLeft: 36
+  }
+});
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -89,10 +134,13 @@ const styles = StyleSheet.create({
     paddingTop: 34,
   },
   title: {
-    backgroundColor: 'red',
     fontSize: 30,
+    textAlign: "center",
   },
   emptyList: {
+    flex: 1,
+    justifyContent: "center",
+    alignSelf: "center",
     fontSize: 25,
   },
   listContainer: {
@@ -102,7 +150,12 @@ const styles = StyleSheet.create({
     borderColor: 'black',
     borderStyle: 'solid',
     borderWidth: 2,
-    fontSize: 20,
     margin: 15
+  },
+  formNewTask: {
+    flex: 1,
+    alignSelf: "center",
+    justifyContent: 'center',
+    width: '50%',
   }
 });
